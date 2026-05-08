45f76644f3e182b7955594f4584b7509d803b5f8
Anirban Das <anirban140963@gmail.com>
2026-05-08 15:09:15 +0530

    refactor: simplify server startup code by defining PORT constant

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,8 @@ app.use("/apps", appRoutes);
 app.use("/data", dataRoutes);
 
 
-app.listen(process.env.PORT  || 8000 , () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT , () => {
+    console.log(`Server running on port ${PORT}`);
 });
